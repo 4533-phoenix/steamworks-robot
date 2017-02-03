463b93f0b2f219ca56724287b98ce10691337862
FRC4533 Developer <jssanders+frc4533@gmail.com>
2017-02-03 17:41:30 -0500

    drive system
    drive system and basic autonomous mode

--- src/org/usfirst/frc/team4533/robot/subsystems/DriveSystem.java
+++ src/org/usfirst/frc/team4533/robot/subsystems/DriveSystem.java
@@ -1,65 +1,105 @@
+
 package org.usfirst.frc.team4533.robot.subsystems;
 
 import org.usfirst.frc.team4533.robot.RobotMap;
+import org.usfirst.frc.team4533.robot.commands.DriveWithJoystick;
 
 import com.ctre.CANTalon;
 import com.ctre.CANTalon.TalonControlMode;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Subsystem;
+
 /**
  *
  */
 public class DriveSystem extends Subsystem {
-	
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
 	private static DriveSystem INSTANCE;
 	
-	CANTalon rightMaster;
-	CANTalon leftMaster; 
-	CANTalon leftSlave;
-	CANTalon rightSlave;
+	private CANTalon leftMaster;
+	private CANTalon rightMaster;
+	private CANTalon leftSlave;
+	private CANTalon rightSlave;
 	
-	/*Note on Master/Slave Drivestyle-
-	 * This works by initiating all four motors (right master & slave, left master & slave), 
-	 * then setting the two slave motors to follow input from their masters
-	 * 
-	 */
-	private DriveSystem(){
-		//this sets the right master motor channel 
-		rightMaster = new CANTalon(RobotMap.RIGHT_MASTER_MOTOR);
-		//this sets the left master motor channel
-		leftMaster = new CANTalon(RobotMap.LEFT_MASTER_MOTOR);
+	private static final double DEFAULT_SPEED_ADJUSTMENT = 1;
+	
+	private DriveSystem() {
+		leftMaster = new CANTalon(RobotMap.Motor_Left_Master);
+		rightMaster = new CANTalon(RobotMap.Motor_Right_Master);
 		
-		//sets the right slave motor channel 
-		rightSlave = new CANTalon(RobotMap.RIGHT_SLAVE_MOTOR);
-		//changes the control mode to follower of the master motor
-		rightSlave.changeControlMode(TalonControlMode.Follower);
-		//sets the master motor to the right master
-		rightSlave.set(RobotMap.RIGHT_MASTER_MOTOR);
-		//sets the left slave motor channel
-		leftSlave = new CANTalon(RobotMap.LEFT_SLAVE_MOTOR);
-		//changes the control mode to be a follower of the master motor 
+		leftSlave = new CANTalon(RobotMap.Motor_Left_Slave);
 		leftSlave.changeControlMode(TalonControlMode.Follower);
-		//sets the master motor to the left master
-		leftSlave.set(RobotMap.LEFT_MASTER_MOTOR);
-	}
-	
-	
-	public void drive(double left, double right){
-		this.leftMaster.set(left);
-		this.rightMaster.set(right);
-		this.leftSlave.set(RobotMap.LEFT_MASTER_MOTOR);
-		this.rightSlave.set(RobotMap.RIGHT_MASTER_MOTOR);
-	}
-
-	public static void initialize(){
+		leftSlave.set(RobotMap.Motor_Left_Master);
 		
-		if(INSTANCE == null){
+		rightSlave = new CANTalon(RobotMap.Motor_Right_Slave);
+		rightSlave.changeControlMode(TalonControlMode.Follower);
+		rightSlave.set(RobotMap.Motor_Right_Master);
+	}
+	
+	public static void initialize() {
+		if (INSTANCE == null) {
 			INSTANCE = new DriveSystem();
 		}
-		
 	}
 	
-	public void initDefaultCommand() {
-	
+	public static DriveSystem getInstance() {
+		return INSTANCE;
 	}
+	
+	public void drive(double left, double right) {
+		this.leftMaster.set(left*DEFAULT_SPEED_ADJUSTMENT);
+		this.leftSlave.set(RobotMap.Motor_Left_Master);
+		this.rightMaster.set(-right*DEFAULT_SPEED_ADJUSTMENT);
+		this.rightSlave.set(RobotMap.Motor_Right_Master);
+	}
+	
+	public void DriveWithJoystick(Joystick driver) {
+		this.drive(driver.getY(), driver.getRawAxis(5));
+	}
+	
+	public void forward(double value) {
+		this.drive(value, value);
+	}
+	
+	public void forward() {
+		this.forward(1.0);
+	}
+	
+	public void backward(double value) {
+		this.drive(value, value);
+	}
+	
+	public void backward() {
+		this.backward(-1.0);
+	}
+	
+	public void stop() {
+		this.drive(0.0, 0.0);
+	}
+	
+	public void turnLeft(double value1, double value2) {
+		this.drive(value1, value2);
+	}
+	
+	public void turnLeft() {
+		this.turnLeft(-.5, .5);
+	}
+
+	public void turnRight(double value1, double value2) {
+		this.drive(value1, value2);
+	}
+	
+	public void turnRight() {
+		this.turnRight(.5, -.5);
+	}
+	
+    public void initDefaultCommand() {
+    	this.setDefaultCommand(new DriveWithJoystick());
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+        }
 }
+
